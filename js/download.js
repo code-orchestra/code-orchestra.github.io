5e6764670b67f60ea180c457cd04c949299f6b1a
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2013-05-09 22:50:17 +0200

    co2 trial button

--- js/download.js
+++ js/download.js
@@ -14,3 +14,17 @@ function download_COLT(){
 		alert("Linux not support yet. Coming soon")
 	}
 }
+
+function download_co2(){
+	var isMac = navigator.platform.toUpperCase().indexOf('MAC')!==-1;
+	var isWindows = navigator.platform.toUpperCase().indexOf('WIN')!==-1;
+	var isLinux = navigator.platform.toUpperCase().indexOf('LINUX')!==-1;
+
+	if(isMac){
+		window.location.replace("http://codeorchestra.com/files/CO2-mac.zip");
+	}else if(isWindows){
+		window.location.replace("http://codeorchestra.com/files/CO2-win.exe");
+	}else if(isLinux){
+		window.location.replace("http://codeorchestra.com/files/CO2-linux.tar.gz");
+	}
+}
