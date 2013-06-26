f7223c6d3247f7db8ecea77692c21cc4e7b3fdb2
supervova <nickysheen@yandex.ru>
2013-06-26 19:45:05 +0200

    Отрезал «бошку»
    Удалил биллборд-комикс.
    Добавил
    твиттер карту,
    опен граф.
    Переписал HTML согласно современным best preacticies
    Перевел стили в LESS

---
+++ README.md
@@ -0,0 +1,4 @@
+Code Orchestra web-site
+=======================
+
+[Try Our Powerful ActionScript Flash Game / Mobile Development Tools](http://goo.gl/c4eDb)
--- js/scripts.js
+++ js/scripts.js
@@ -1,123 +1,168 @@
-// JavaScript Document
-$(document).ready(function() {
-    var enableAnimations = (navigator.appVersion.indexOf("MSIE 8") == -1 /*&& navigator.appVersion.indexOf("Windows NT 5"*) == -1*/) ? true : false;
-    
-    $(".dialog").dialog({
-        modal: true,
-        autoOpen: false,
-        width: 1280,
-        closeOnEscape: true,
-        close: function(e, ui) {
-          $(".dialog").empty();
-        }
-    });
-    
-    
-    $(".ui-widget-overlay, .ui-widget-content").live("click touchstart", function() {
-      $(".dialog").dialog("close");
-      return false;
-    });
-    
-    $(".dialog-link").live("click", function() {
-      var hrefVal = $(this).attr("data-dialog"),
-          isVideo = hrefVal.indexOf("vimeo") != -1 ? true : false,
-          isImage = hrefVal.indexOf("assets/images") != -1 ? true : false;
-      $(".dialog").empty();
-      
-      if (isVideo) {
-        $(".dialog").dialog("option", "height", 430).html('<iframe src="' + hrefVal + '" width="640" height="400" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>');
-      }
-      else if (isImage) {
-        $(".dialog").dialog("option", "height", 765).html('<img src="' + hrefVal + '" title="" alt="" />');
-      }
-      
-      $(".dialog").dialog("open");
-      
-      if (navigator.userAgent.match(/iPad/i)) {
-        $(".ui-dialog ").css("top", (parseInt($(".ui-dialog").css("top"), 10) + 50) + "px");
-      }
-      return false;
-    });
-    
-    
-    
-    //Share buttons handlers
-    $("li.social a").on("click", function() {
-        var $this = $(this),
-            parentElm = $(this).parent(),
-            url = encodeURIComponent(window.location),
-            txt = $('meta[property="og:title"]').attr("content");
-        
-        if (parentElm.hasClass("facebook")) {
-            window.open("http://www.facebook.com/sharer.php?u=" + url + "&t=" + txt, "sharer", "toolbar=0,status=0,width=626,height=436");
-        }
-        else if (parentElm.hasClass("twitter")) {
-            window.open("http://twitter.com/share?url=" + url + "&text=@F_i newest case study on their own iconography ");
-        }
-        else if (parentElm.hasClass("google-plus")) {
-            window.open("https://plus.google.com/share?url=" + url, "", "menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600");
-        }
-        
-        return false;
-    });
-    
-    //Animations on images
-    $(".image-animation").bind("inview", function (event, visible) {
-        var $this = $(this);
-        if (visible == true) {
-            if ($this.hasClass("animate-consecutive")) {
-                if ($this.hasClass("sony-animation")) {
-                    //handle sony animation
-                    setTimeout(function () {
-                        if (enableAnimations) {
-                            $this.find(".decoration-icon").css('visibility','visible').animate({left: "315px", opacity: 1}, 300);
-                        }
-                        else {
-                            $this.find(".decoration-icon").css({visibility: 'visible', left: "315px"}).hide().effect("fade", {}, 300);
-                        }
-                    }, 300);
-                }
-                else if ($this.hasClass("ramayana-animation")) {
-                    //handle ramayana animation
-                    setTimeout(function () {
-                        if (enableAnimations) {
-                            $this.find(".animated").eq(0).css('visibility','visible').animate({opacity: 1}, 300);
-                            $this.find(".animated").eq(1).css('visibility','visible').animate({top: "147px", left: "240px", opacity: 1}, 300);
-                            $this.find(".animated").eq(2).css('visibility','visible').animate({left: "247px", opacity: 1}, 300);
-                        }
-                        else {
-                            $this.find(".animated").eq(0).css({visibility: 'visible'}).hide().effect("fade", {}, 300);
-                            $this.find(".animated").eq(1).css({visibility: 'visible', top: "147px", left: "240px"}).hide().effect("fade", {}, 300);
-                            $this.find(".animated").eq(2).css({visibility: 'visible', left: "247px"}).hide().effect("fade", {}, 300);
-                        }
-                    }, 500);
-                }
-                else {
-                    //handle all other animated elements
-                    var iteration = 1,
-                        delayInMilisec = 300;
-                        totalAnimatedElements = $this.find(".animated").length;
-                    $this.find(".animated").each(function() {
-                        var elm = $(this);
-                        
-                        if (totalAnimatedElements > 2 && iteration > 2 && delayInMilisec >= 30) {
-                            delayInMilisec -= 30;
-                        }
-                        
-                        setTimeout(function () {
-                            elm.css('visibility','visible').hide().effect("fade", {}, 300);
-                        }, iteration*delayInMilisec);
-                        iteration++;
-                    });
-                }
+/*
+|-------------------------------------------------------------------------------
+| CALLING PLUGINS FUNCTIONS
+|-------------------------------------------------------------------------------
+|
+*/
+$(function () {
+    $('#titul-slides').slides({
+        preload: true,
+        preloadImage: 'images/loading.gif',
+        play: 5000,
+        pause: 2500,
+        hoverPause: true,
+        animationStart: function (current) {
+            $('.caption').animate({
+                opacity: 0
+            }, 100);
+            $('#slides-header').animate({
+                opacity: 0
+            }, 100);
+            if (window.console && console.log) {
+                // example return of current slide number
+                console.log('animationStart on slide: ', current);
             }
-            
-            $this.unbind("inview");
-        } else {
-          // element is out of viewport
+        },
+        animationComplete: function (current) {
+            $('.caption').animate({
+                opacity: 1.0
+            }, 200);
+            $('#slides-header').css('background-image', 'url(images/text' + current + '.png)');
+            $('#slides-header').animate({
+                opacity: 1.0
+            }, 200);
+            if (window.console && console.log) {
+                // example return of current slide number
+                console.log('animationComplete on slide: ', current);
+            }
+        },
+        slidesLoaded: function () {
+            $('.caption').animate({
+                opacity: 1.0
+            }, 200);
         }
     });
-    
-    //call scroll event if the elements are already in visible part of the browser
-    $(window).scroll();
 });
+
+$(document).ready(function () {
+    $("#tabsCo2").verticaltabs({
+        speed: 500,
+        slideShow: false,
+        activeIndex: 0
+    });
+});
+
+$(document).ready(function () {
+    $("a[rel^='prettyPhoto']").prettyPhoto();
+});
+
+$(window).load(function () { //$(window).load() must be used instead of $(document).ready() because of Webkit compatibility
+    $("#carousel-ytube").sliderkit({
+        auto: true,
+        autospeed: 4000,
+        shownavitems: 5,
+        circular: true,
+        fastchange: false,
+        scrolleasing: "easeOutExpo", //"easeOutBounce, easeOutBack"
+        scrollspeed: 500
+    });
+});
+
+/*
+|-------------------------------------------------------------------------------
+| CONTEST
+|-------------------------------------------------------------------------------
+|
+*/
+$(document).ready(function(){
+	$("#contest-open").click(function(){
+		$("#ide-wrapper .ide-container").css('padding-top','1940px');
+		$("#ide-images").css('top','1675px');
+		$("#contest").fadeIn('slow')
+	});
+	$("#contest .contest-close").click(function(){
+		$("#ide-wrapper .ide-container").css('padding-top','460px');
+		$("#ide-images").css('top','195px');
+		$("#contest").fadeOut('fast')
+	});
+	
+	$("#action-open").click(function(){
+		$("#action-gifs").slideDown('slow');
+		$("#action-open").fadeOut('fast');
+	});
+	$("#action-gifs .action-close").click(function(){
+		$("#action-gifs").slideUp('fast');
+		$("#action-open").fadeIn('fast');
+	});
+	
+	$("#download-colt-trial-button").click(function(){
+		download_COLT();
+	});
+	
+	$("#download-co2-trial-button").click(function(){
+		download_co2();
+	});
+	
+	
+	$("#rocket-div").click(function(e){
+		// ok, so people want to click stuff in that div
+		// we don't want to mess with its layout, so here goes the hack
+		if(e.target.id != "rocket-div") {
+			// ignore logo images in the div
+			return;
+		}
+		var offset = $(e.target).offset();
+		var x = e.pageX - offset.left;
+		if((x < 285)||(x > 690)) {
+			// find closest "appears to be a link" area in the div
+			var tabs = {
+				ts:{x:175,y:115},
+				md:{x:815,y:125},
+				nr:{x:125,y:215},
+				tw:{x:810,y:210},
+				st:{x:145,y:310},
+				dp:{x:845,y:300}
+			}
+			var y = e.pageY - offset.top, d = 1e23, t;
+			for (var tab in tabs) {
+				var ptab = tabs[tab];
+				var dtab = (x-ptab.x)*(x-ptab.x)+(y-ptab.y)*(y-ptab.y);
+				if (dtab < d) { d = dtab; t = tab; }
+			}
+			// trigger click handler in corresponding tab
+			$(".tabs-"+t+" a").trigger("click");
+			// scroll down to the tabs
+			// (thanks to that top menu we need to go to something above the tabs)
+			location = "#download-colt-trial-button";
+		}
+	});
+	if(location.hash == '#contest-open'){
+		$('#contest-open').trigger('click');
+	};
+});
+
+/*
+|-------------------------------------------------------------------------------
+| STICKY MENU
+|-------------------------------------------------------------------------------
+|
+*/
+$(function() {
+	$(window).scroll(function(){
+		/* when reaching the element with id "last" we want to show the slidebox. Let's get the distance from the top to the element */
+		var distanceTop = $(window).height();
+		
+		if  ($(window).scrollTop() > distanceTop)
+			$('#slidepanel').animate({'top':'0px'},400);
+		else 
+			$('#slidepanel').stop(true).animate({'top':'-61px'},200);	
+	});
+	
+	
+	
+	/* remove the slidebox when clicking the cross */
+	/*$('#slidepanel').bind('click',function(){
+		$(this).parent().remove();
+	});*/
+});
+
